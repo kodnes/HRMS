9d4e698694f1531a94285770f70787b09c70fded
harsha <edigaharsha7374@gmail.com>
2024-05-02 16:14:05 +0530

    hi

---
+++ Documents/workspace-spring-tool-suite/EmployeeServices/src/test/java/com/EurekaServer/EmployeeServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
